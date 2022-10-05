389e0e96e44b083d9ad88dc9c4ecc07377a48297
FranciscoJManasAlvarez <fjmanas@dia.uned.es>
2022-10-05 10:04:21 +0200

    New pkg to Gazebo and Webots

---
+++ uned_vicon_sim/uned_vicon_sim/vicon_gazebo.py
@@ -0,0 +1,6 @@
+def main():
+    print('uned_vicon_sim::Gazebo Driver In progress ...')
+
+
+if __name__ == '__main__':
+    main()
---
+++ uned_vicon_sim/uned_vicon_sim/vicon_webots.py
@@ -0,0 +1,6 @@
+def main():
+    print('uned_vicon_sim::Webots Driver In progress ...')
+
+
+if __name__ == '__main__':
+    main()
