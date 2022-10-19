2f7ba8e7c5d96bcd4d175590e4718d9c61dfd238
FranciscoJManasAlvarez <fjmanas@dia.uned.es>
2022-10-19 14:23:57 +0200

    Add TF

--- uned_vicon_sim/uned_vicon_sim/vicon_webots.py
+++ uned_vicon_sim/uned_vicon_sim/vicon_webots.py
@@ -1,7 +1,8 @@
 import rclpy
-
 from rclpy.node import Node
-from geometry_msgs.msg import Pose, PointStamped
+from geometry_msgs.msg import Pose, PointStamped, TransformStamped
+from tf2_ros import TransformBroadcaster
+import tf_transformations
 
 agent_list = list()
 
@@ -10,7 +11,7 @@ class Agent():
         self.id = id
         self.pose = Pose()
         self.parent = parent
-        self.sub_pose_ = self.parent.create_subscription(PointStamped, self.id + '/gps', self.gtpose_callback, 10)
+        self.sub_pose_ = self.parent.create_subscription(PointStamped, self.id + '/driver/gps', self.gtpose_callback, 10)
         self.publisher_ = self.parent.create_publisher(Pose, self.id + '/pose', 10)
 
     def gtpose_callback(self, msg):
@@ -19,12 +20,35 @@ class Agent():
         self.pose.position.z = msg.point.z
         self.publisher_.publish(self.pose)
 
+        t = TransformStamped()
+        # Read message content and assign it to
+        # corresponding tf variables
+        t.header.stamp = self.parent.get_clock().now().to_msg()
+        t.header.frame_id = 'world'
+        t.child_frame_id = self.id
+        t.transform.translation.x = msg.point.x
+        t.transform.translation.y = msg.point.y
+        t.transform.translation.z = msg.point.z
+
+        q = tf_transformations.quaternion_from_euler(0, 0, 0)
+        t.transform.rotation.x = q[0]
+        t.transform.rotation.y = q[1]
+        t.transform.rotation.z = q[2]
+        t.transform.rotation.w = q[3]
+
+        self.parent.br.sendTransform(t)
+
 class ViconWebots(Node):
 
     def __init__(self):
         super().__init__('vicon_webots')
         self.declare_parameter('agents', 'robot01')
+
+        # Initialize the transform broadcaster
+        self.br = TransformBroadcaster(self)
+
         self.initialize()
+        
 
     def initialize(self):
         self.get_logger().info('Vicon Webots::inicialize() ok.')
@@ -34,6 +58,7 @@ class ViconWebots(Node):
             agent_str = id_array[i-1]
             robot = Agent(self, agent_str)
             agent_list.append(robot)
+        
 
 
 def main(args=None):
